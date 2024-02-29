4031513f8a41e16aa145d34ddc85518b6c0a2a84
Davchick <davchick2000ort@gmail.com>
2024-02-29 23:31:02 +0300

    fix: remove unnecessary imports of "React"

--- src/pages/Cooperation.tsx
+++ src/pages/Cooperation.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Cooperation = () => {
   return (
     <div>Cooperation</div>
--- src/pages/TestDrive.tsx
+++ src/pages/TestDrive.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const TestDrive = () => {
   return (
     <div>TestDrive</div>
